c2034dee6965aae3af170ae8fe3287f4a5622727
Anshul Rana <anshulrana2510@gmail.com>
2023-08-20 02:13:52 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import {
+  Link,
+  Router,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+function App() {
+  return (
+    <h2 style={{display:"flex",justifyContent:"center"}}>
+      Hi ✌, I am Anshul Rana 
+    </h2>
+  );
+}
+
+export default App;
